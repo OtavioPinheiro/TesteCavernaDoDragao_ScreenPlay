97e9bf8ff8e79dc08e3cb8a9cc086d575ae4a50a
Otávio Augusto Pinheiro <49002375+OtavioPinheiro@users.noreply.github.com>
2021-07-25 22:56:58 -0300

    feat - GlobalDriver

--- src/main/java/br/com/cavernadodragao/utils/GlobalDriver.java
+++ src/main/java/br/com/cavernadodragao/utils/GlobalDriver.java
@@ -1,9 +1,20 @@
 package br.com.cavernadodragao.utils;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 
 public class GlobalDriver {
 
-    private static WebDriver driver = null;
+    private static WebDriver driver;
+//    private static final String chosenBrowser = System.getenv("BROWSER");
 
+    public static WebDriver initiateDriver() throws Exception {
+        System.setProperty("webdriver.chrome.driver", Environments.get("chrome"));
+        driver = new ChromeDriver();
+        return driver;
+    }
+
+    public static WebDriver getBrowser(){
+        return driver;
+    }
 }
