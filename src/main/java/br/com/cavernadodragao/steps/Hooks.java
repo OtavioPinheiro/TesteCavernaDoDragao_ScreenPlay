890397d70546956c28fd2eaaf21380ebcda9c1a2
Otávio Augusto Pinheiro <49002375+OtavioPinheiro@users.noreply.github.com>
2021-07-25 23:14:02 -0300

    feat - Hooks

---
+++ src/main/java/br/com/cavernadodragao/steps/Hooks.java
@@ -0,0 +1,23 @@
+package br.com.cavernadodragao.steps;
+
+import br.com.cavernadodragao.utils.GlobalDriver;
+import io.cucumber.java.After;
+import io.cucumber.java.Before;
+import io.cucumber.java.Scenario;
+import io.cucumber.plugin.event.PickleStepTestStep;
+
+public class Hooks {
+//    PickleStepTestStep currentStep;
+//    private int counter = 0;
+
+    @Before
+    public void beforeEveryScenario(Scenario scenario) throws Exception {
+        GlobalDriver.getDriver();
+        GlobalDriver.maximizeWindow();
+    }
+
+    @After
+    public void afterEveryScenario() throws  Exception {
+        GlobalDriver.closeDriver();
+    }
+}
