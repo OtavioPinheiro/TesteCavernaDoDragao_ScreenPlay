627f2644852326a0d917f91e3320c80f784e5b86
Otávio Augusto Pinheiro <49002375+OtavioPinheiro@users.noreply.github.com>
2021-07-25 23:29:09 -0300

    feat - Commons

--- src/main/java/br/com/cavernadodragao/actions/CommonActions.java
+++ src/main/java/br/com/cavernadodragao/actions/CommonActions.java
@@ -1,4 +1,12 @@
 package br.com.cavernadodragao.actions;
 
+import br.com.cavernadodragao.utils.GlobalDriver;
+import org.openqa.selenium.WebDriver;
+
 public abstract class CommonActions {
+    private final WebDriver web = GlobalDriver.getDriver();
+
+    public CommonActions() throws Exception {}
+
+
 }
