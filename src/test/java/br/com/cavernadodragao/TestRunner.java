d6dbecbc05fae647362f8681e780095b50c889fc
Otávio Augusto Pinheiro <49002375+OtavioPinheiro@users.noreply.github.com>
2021-07-26 21:30:29 -0300

    feat - TestRunner

--- src/test/java/br/com/cavernadodragao/TestRunner.java
+++ src/test/java/br/com/cavernadodragao/TestRunner.java
@@ -1,4 +1,15 @@
 package br.com.cavernadodragao;
 
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        features = "./src/main/java/br/com/cavernadodragao/requirements",
+        glue = "br.com.cavernadodragao.steps",
+        monochrome = true
+)
+
 public class TestRunner {
 }
