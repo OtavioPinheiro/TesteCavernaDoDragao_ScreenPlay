e2ba26c586b9986ac4937093f34edf10e7232b6f
Otávio Augusto Pinheiro <49002375+OtavioPinheiro@users.noreply.github.com>
2021-07-26 21:21:21 -0300

    feat - CommonQuestions

--- src/main/java/br/com/cavernadodragao/questions/CommonQuestions.java
+++ src/main/java/br/com/cavernadodragao/questions/CommonQuestions.java
@@ -1,4 +1,10 @@
 package br.com.cavernadodragao.questions;
 
+import org.openqa.selenium.WebElement;
+
 public abstract class CommonQuestions {
+
+    public boolean hasDisplayedElement(WebElement element) throws Exception {
+        return element.isDisplayed();
+    }
 }
